53482c2a3e6e0db1ac2839073eaa09f0629bf86e
Humanz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-12-17 15:34:28 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,36 +1,14 @@
-let { generateWAMessageFromContent, prepareWAMessageMedia, proto } = (await import('@adiwajshing/baileys')).default
-import moment from 'moment-timezone'
-import fetch from 'node-fetch'
-import fs from 'fs'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-const messa = await prepareWAMessageMedia({ image: fs.readFileSync('./media/ok.jpg') }, { upload: conn.waUploadToServer })
-const catalog = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-"productMessage": {
-"product": {
-"productImage": messa.imageMessage, 
-"productId": "5489299044451325",
-"title": `Jasa Sewa GojoBotzMD✓`,
-"description": `HALO BANG`,
-"currencyCode": "IDR",
-"bodyText": wm,
-"footerText": wm,
-"priceAmount1000": "3000",
-"productImageCount": 1,
-"firstImageId": 1,
-"salePriceAmount1000": "10000000",
-"retailerId": wm,
-"url": "wa.me/6288804947282"
-},
-"businessOwnerJid": "6288804947282@s.whatsapp.net",
-}
-}), { userJid: m.chat, quoted: ftroli })    
-
-conn.relayMessage(m.chat, catalog.message, { messageId: catalog.key.id })
-}
+let handler =  m => m.reply(`
+╭─「 Donasi • Pulsa 」
+│ • Indosat Ooredoo [085713964963]
+│ • Smartfren [088804947282]
+╰────
+╭─「 Donasi • Non Pulsa 」
+│ • Dana [088804947282]
+╰────
+`.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
-handler.tags = ['main']
-handler.command = /^(donasi|donate)$/i
-
-handler.limit = true
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
 
 export default handler
