538e27fbd723cea976034db45da9789c43562ecd
Humanz <109642842+OrochimaruBots@users.noreply.github.com>
2022-12-25 18:31:55 +0700

    Update fun-jo.js

--- plugins/fun-jo.js
+++ plugins/fun-jo.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch'
 let handler = async (m, {text, args}) => {
   if (!args[0]) throw `Use example .jo halo`
-  let api = await fetch(`https://simsimi.info/api/?text=${text}&lc=id`)
-  let res = await api.json()
+  let api = await fetch(`https://saipulanuar.ga/api/f/simi?apikey=APIKEY&query=simii`)
+  let res = await api.json(`https://saipulanuar.ga/api/f/simi?apikey=APIKEY&query=simii`)
   m.reply(res.success)
 }
 handler.command = ['jo']
